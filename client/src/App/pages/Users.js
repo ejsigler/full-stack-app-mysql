8a6ffe9ceda73f988ce4b25de0e64256b0edf5ab
Sunil M <sunil-nonadmin@Sunils-MacBook-Air.local>
2018-10-22 13:35:03 -0400

    first commit of the full stack ap

--- client/src/App/pages/Users.js
+++ client/src/App/pages/Users.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class List extends Component {
   // Initialize the state
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       list: []
@@ -17,8 +17,8 @@ class List extends Component {
   // Retrieves the list of items from the Express app
   getList = () => {
     fetch('/api/users')
-    .then(res => res.json())
-    .then(list => this.setState({ list }))
+      .then(res => res.json())
+      .then(list => this.setState({ list }))
   }
 
   render() {
@@ -32,80 +32,46 @@ class List extends Component {
           <div>
             <table className="table table-hover">
               <thead>
-                  <tr>
-                      <th>First Name</th>
-                      <th>Last Name</th>
-                      <th>UserName</th>
-                      <th>Email</th>
-                      <th>Phone number</th>
-                      <th>Github Handle</th>
-                      <th>Action</th>
-                  </tr>
+                <tr>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>UserName</th>
+                  <th>Email</th>
+                  <th>Phone number</th>
+                  <th>Github Handle</th>
+                  <th>Action</th>
+                </tr>
               </thead>
-                  
-            {/* Render the list of items */}
+
+              {/* Render the list of items, ensure that key values match the columns names in the db */}
               <tbody>
-              {list.map((item) => {
-                return(
-                    <tr key={item.UserId}>
-                      <td>{item.FirstName} </td>
-                      <td>{item.LastName}</td>
-                      <td>{item.UserName}</td>
-                      <td>{item.Email}</td>
-                      <td>{item.Phone}</td>
-                      <td>{item.GithubHandle}</td>
+                {list.map((item) => {
+                  return (
+                    <tr key={item.userId}>
+                      <td>{item.firstName} </td>
+                      <td>{item.lastName}</td>
+                      <td>{item.userName}</td>
+                      <td>{item.email}</td>
+                      <td>{item.phone}</td>
+                      <td>{item.githubHandle}</td>
                       <td><a>Edit</a>|<a>Delete</a></td>
                     </tr>
-                    )}
                   )
+                }
+                )
                 }
               </tbody>
             </table>
           </div>
         ) : (
-          <div>
-            <h2>No List Items Found</h2>
-          </div>
-        )
-      }
+            <div>
+              <h2>No List Items Found</h2>
+            </div>
+          )
+        }
       </div>
     );
   }
-  /*
-  render() {
-    const { list } = this.state;
-
-    return (
-      <div className="App">
-        <h1>List of Items</h1>
-        {/* Check to see if any items are found*//*}
-        {list.length ? (
-          <div>
-            {/* Render the list of items *//*}
-            {list.map((item) => {
-              return(
-                <div>
-                  {item.UserID}
-                  {item.FirstName}
-                  {item.LastName}
-                  {item.UserName}
-                  {item.Email}
-                  {item.Phone}
-                  {item.GithubHandle}
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div>
-            <h2>No List Items Found</h2>
-          </div>
-        )
-      }
-      </div>
-    );
-  }
-  */
 }
 
 export default List;
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+const mysql = require('mysql2');
 const Sequelize = require('Sequelize');
-const sequelize = new Sequelize('Portfolio', 'michael', null, {
-    host: 'localhost',
-    dialect: 'sqlite',
-    storage: 'Chinook_Sqlite_AutoIncrementPKs.sqlite'
-});
 
+//Uncomment the below to use with local host
+/*const sequelize = new Sequelize('developer_portfolio', 'root', null, {
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql',
+    //storage: 'Chinook_Sqlite_AutoIncrementPKs.sqlite'
+});*/
+
+//Use the below for RDS
+//1. update host name to the RDS endpoint
+//2. Ensure that the VPC security group setup in RDS allows all inbound traffic
+const sequelize = new Sequelize('developer_portfolio', 'admin', 'admin1234', {
+    host: 'mydbinstance.cmoqkjgljhtw.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    dialect: 'mysql',
+});
 
 const app = express();
 
@@ -18,27 +29,31 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-let db = new sqlite3.Database('Chinook_Sqlite_AutoIncrementPKs.sqlite', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the chinook database.');
-  });
+//Test the Connection
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
-const Developers = sequelize.define(
-    'Developers',
+//Define the model
+const developers = sequelize.define(
+    'developers',
     {
-        UserId: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+        userId: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
         },
-        FirstName: Sequelize.STRING,
-        LastName: Sequelize.STRING,
-        UserName: Sequelize.STRING,
-        Email: Sequelize.STRING,
-        Phone: Sequelize.STRING,
-        GitHubHandle: Sequelize.STRING
+        firstName: Sequelize.STRING,
+        lastName: Sequelize.STRING,
+        userName: Sequelize.STRING,
+        email: Sequelize.STRING,
+        phone: Sequelize.STRING,
+        gitHubHandle: Sequelize.STRING
     },
     {
         freezeTableName: true,
@@ -46,17 +61,10 @@ const Developers = sequelize.define(
     }
 );
 
-// An api endpoint that returns a short list of items
-//app.get('/api/getList', (req,res) => {
-app.get('/api/users', (req,res) => {
-    Developers.findAll()
-    .then(users => {res.json(users)})
-    /*Developers.findAll({
-        where: {
-            UserId: parseInt(req.params.userId)
-        }
-    }).then(list => {res.json(list),
-    console.log(res.json(list))});
+// An api endpoint that returns a list of all users
+app.get('/api/users', (req, res) => {
+    developers.findAll()
+        .then(users => { console.log(users); res.json(users) })
     /*
     var list = ["item1", "item2", "item3"];
     res.json(list);
@@ -64,11 +72,20 @@ app.get('/api/users', (req,res) => {
     console.log('Sent list of items');
 });
 
-db.close();
+// An api endpoint that returns a user
+app.get('/api/:username', (req, res) => {
+    developers.findOne({
+        where: {
+            UserName: parseInt(req.params.username)
+        }
+    })
+        .then(user => { res.json(user) })
+    console.log('Sent list of items');
+});
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
